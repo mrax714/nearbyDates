8e58ab5682eabe9f3a891d7a502aa8720294fbca
githubertus <github@spam-s.cked.de>
2016-09-25 17:35:55 +0200

    Create logged_in_index.php

---
+++ pages/logged_in_index.php
@@ -0,0 +1,13 @@
+<div class="row ossn-page-contents">
+		<div class="col-md-6 left-contents">
+			Hello Index HTML 
+			<br />
+			Congrats! You've successfully logged in now.
+			<br />
+			<br />
+			some space to fill (left column)
+			</div>   
+        <div class="col-md-6">
+			and some more space (right column)
+		</div>     
+</div>	
