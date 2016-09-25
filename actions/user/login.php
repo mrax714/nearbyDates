e3cd8e3b74d47980949409414e7c957b00037485
githubertus <github@spam-s.cked.de>
2016-09-25 17:33:04 +0200

    Create login.php

---
+++ actions/user/login.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2016 SOFTLAB24 LIMITED
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+
+if(ossn_isLoggedin()) {
+		// redirect('home');
+		// redirect to custom index page instead
+		redirect();
+}
+$username = input('username');
+$password = input('password');
+
+if(empty($username) || empty($password)) {
+		ossn_trigger_message(ossn_print('login:error'));
+		redirect();
+}
+$user = ossn_user_by_username($username);
+
+//check if username is email
+if(strpos($username, '@') !== false) {
+		$user     = ossn_user_by_email($username);
+		$username = $user->username;
+}
+
+if($user && !$user->isUserVALIDATED()) {
+		$user->resendValidationEmail();
+		ossn_trigger_message(ossn_print('ossn:user:validation:resend'), 'error');
+		redirect(REF);
+}
+$vars = array(
+		'user' => $user
+);
+ossn_trigger_callback('user', 'before:login', $vars);
+
+$login           = new OssnUser;
+$login->username = $username;
+$login->password = $password;
+if($login->Login()) {
+		//One uneeded redirection when login #516
+		ossn_trigger_callback('login', 'success', $vars);
+		// redirect('home');
+		// redirect to custom index page instead
+		redirect();
+} else {
+		redirect('login?error=1');
+}
